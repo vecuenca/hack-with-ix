ed2a75c608ccda590b1957e9d653036e206f695a
Jack Yiu <jackinyiu@gmail.com>
2016-11-20 09:39:01 -0500

    Fixed total spend

--- ui/src/components/impression/TotalSpend.js
+++ ui/src/components/impression/TotalSpend.js
@@ -3,31 +3,22 @@ import React, {Component} from 'react';
 class TotalSpend extends Component {
   calculateTotalSpend() {
     var total = 0;
-    var counter = 0;
+
     this.props.impressions[this.props.datacenter].forEach(function (i) {
-      counter+=1;
-      total += i.spend;
-    });
-    return total/counter;
-  }
-  calculateTotalImpressions() {
-  var total = 0;
-    this.props.impressions[this.props.datacenter].forEach(function(i) {
-      total += i.impressions;
+      var cpm = i.spend / i.impressions;
+      total += (i.impressions/1000) * cpm
     });
     return total;
   }
+  
 
   render() {
     if (this.props.impressions && this.props.impressions[this.props.datacenter] ) {
       const totalSpend = Math.round(this.calculateTotalSpend());
-      const totalImpressions = Math.round(this.calculateTotalImpressions());
-
-      const totalMONEY = (totalSpend * (totalImpressions / 1000))/ 100;
 
       return (
         <div style={style.root}>
-          <p style={style.bigValue}>{Math.round(totalMONEY/1000000)}M</p>
+          <p style={style.bigValue}>{Math.round(totalSpend/1000)*1000}</p>
           <p style={style.postText}>Dollars Spent<br /> on advertising</p>
         </div>
       );
