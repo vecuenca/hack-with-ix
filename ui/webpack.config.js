05c6ae67ea7ac3e74420235be2d27ebd123f243a
Kenny Lam <kenny.lam@mail.utoronto.ca>
2016-11-20 02:33:35 -0500

    added a better flexbox grid

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -22,7 +22,12 @@ module.exports = {
         test: /\.js$/,
         loaders: [ 'react-hot', 'babel' ],
         include: path.join(__dirname, 'src')
-      }
+      },
+	{
+	  test: /\.css$/,
+	  loader: 'style!css?modules',
+	  include: /flexboxgrid/,
+	}
     ]
   },
   resolve: {
